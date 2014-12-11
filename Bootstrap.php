d4ec8676f8288d2dd4408f2e53348c84f8065589
callmez <callme-z@qq.com>
2014-12-11 20:23:41 +0800

    Boostrap功能

---
+++ Bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+namespace callmez\wechat;
+
+use yii\base\BootstrapInterface;
+
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        
+    }
+}

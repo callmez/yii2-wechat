ec38f5a62132765278bec54a5a741d62574ae8db
callmez <callme-z@qq.com>
2015-07-01 11:31:29 +0800

    代码清理

--- Bootstrap.php
+++ Bootstrap.php
@@ -19,14 +19,14 @@ class Bootstrap implements BootstrapInterface
         if ($app->hasModule('gii')) { // 增加gii生成器
             $gii = $app->getModule('gii');
 
-            if (array_key_exists('ajaxcrud', $gii->generators)) { // 微信生成器
-                $gii->generators['ajaxcrud'] = [
-                    'class' => 'callmez\ajaxcrud\Generator'
+            if (array_key_exists('wechat', $gii->generators)) { // 微信生成器
+                $gii->generators['wechat'] = [
+                    'class' => 'callmez\wechat\Generator'
                 ];
             }
 
-            if (array_key_exists('wechat', $gii->generators)) { // ajax crud
-                $gii->generators['wechat'] = [
+            if (array_key_exists('ajaxcrud', $gii->generators)) { // AJAX-CRUD
+                $gii->generators['ajaxcrud'] = [
                     'class' => 'callmez\ajaxcrud\Generator'
                 ];
             }

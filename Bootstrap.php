4b55f8df1e9c44acde4348e14202daf4b549c6f2
callmez <callme-z@qq.com>
2015-07-03 14:14:16 +0800

    require kartik-v/yii2-grid, kartik-v/yii2-datecontrol

--- Bootstrap.php
+++ Bootstrap.php
@@ -2,6 +2,7 @@
 
 namespace callmez\wechat;
 
+use Yii;
 use yii\base\Application;
 use yii\base\BootstrapInterface;
 
@@ -16,20 +17,32 @@ class Bootstrap implements BootstrapInterface
      */
     public function bootstrap($app)
     {
+        // 定义毫秒时间戳
+        defined('TIMESTAMP') or define('TIMESTAMP', $_SERVER['REQUEST_TIME']);
+        // 自动注册存储目录(可以在config中配置)
+        isset(Yii::$aliases['@storage']) or Yii::setAlias('@storage', Yii::getAlias('@web/storage'));
+        isset(Yii::$aliases['@storageRoot']) or Yii::setAlias('@storageRoot', Yii::getAlias('@webroot/storage'));
+
         if ($app->hasModule('gii')) { // 增加gii生成器
             $gii = $app->getModule('gii');
 
-            if (array_key_exists('wechat', $gii->generators)) { // 微信生成器
+            if (!array_key_exists('wechat', $gii->generators)) { // 微信生成器
                 $gii->generators['wechat'] = [
-                    'class' => 'callmez\wechat\Generator'
+                    'class' => 'callmez\wechat\generators\module\Generator'
                 ];
             }
 
-            if (array_key_exists('ajaxcrud', $gii->generators)) { // AJAX-CRUD
+            if (!array_key_exists('ajaxcrud', $gii->generators)) { // AJAX-CRUD
                 $gii->generators['ajaxcrud'] = [
-                    'class' => 'callmez\ajaxcrud\Generator'
+                    'class' => 'callmez\ajaxcrud\generators\crud\Generator'
                 ];
             }
         }
+        if (!$app->hasMethod('gridview')) { // 设置 GridView模块
+            $app->setModule('gridview', [
+                'class' => '\kartik\grid\Module'
+            ]);
+        }
+
     }
 }
--- views/wechat/index.php
+++ views/wechat/index.php
@@ -1,86 +1,144 @@
 <?php
 
 use yii\helpers\Html;
+use kartik\grid\GridView;
 use callmez\wechat\models\Wechat;
-use callmez\wechat\widgets\GridView;
 use callmez\wechat\widgets\PagePanel;
 
 $this->title = '公众号列表';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<?php PagePanel::begin(['options' => ['class' => 'wechat-index']]) ?>
-    <p>
-        <?= Html::a('添加公众号', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            [
-                'attribute' => 'id',
-                'options' => [
-                    'width' => 75
-                ]
-            ],
-            'name',
-//            'token',
-//            'access_token',
-//            'account',
-//            'original',
-            [
-                'attribute' => 'type',
-                'format' => 'html',
-                'value' => function($model) {
-                    return Html::tag('span', Wechat::$types[$model->type], [
-                        'class' => 'label label-info'
-                    ]);
-                },
-                'filter' => Wechat::$types,
+<?= GridView::widget([
+    'pjax' => true,
+    'condensed' => true,
+    'responsiveWrap' => false,
+    'panel' => [
+        'type' => 'default',
+        'heading' => '<i class="glyphicon glyphicon glyphicon-list"></i> ' . $this->title,
+        'before' => Html::a('添加公众号', ['create'], ['class' => 'btn btn-success', 'data-pjax' => 0]),
+    ],
 
-            ],
-//            'key',
-//            'secret',
-//            'encoding_aes_key',
-//            'avatar',
-//            'qrcode',
-//            'address',
-//            'description',
-//            'username',
-//            'status',
-//            'password',
-            [
-                'attribute' => 'status',
-                'format' => 'html',
-                'value' => function($model) {
-                    return Html::tag('span', Wechat::$statuses[$model->status], [
-                        'class' => 'label label-' . ($model->status == Wechat::STATUS_ACTIVE ? 'success' : 'danger')
-                    ]);
-                },
-                'filter' => Wechat::$statuses,
-            ],
-            'created_at:datetime',
-            'updated_at:datetime',
-            [
-                'class' => 'callmez\wechat\widgets\ActionColumn',
-                'template' => '{manage} {update} {delete}',
-                'buttons' => [
-                    'manage' => function ($url, $model) {
-                        return Html::a('管理此公众号', $url, [
-                            'class' => 'text-danger',
-                            'data' => [
-                                'toggle' => 'tooltip',
-                                'placement' => 'bottom'
-                            ],
-                            'title' => '管理此公众号'
-                        ]);
-                    }
-                ]
-            ]
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+        [
+            'class' => 'kartik\grid\CheckboxColumn',
+            'width' => '30px',
         ],
-    ]); ?>
-<?php PagePanel::end() ?>
-<?php
-$this->registerJs(<<<EOF
-    $('[data-toggle="tooltip"]').tooltip();
-EOF
-);
+
+        [
+            'class' => 'kartik\grid\DataColumn',
+            'attribute' => 'id',
+            'width' => '20px',
+        ],
+        [
+            'class' => 'kartik\grid\DataColumn',
+            'attribute' => 'name'
+        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'token'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'access_token'
+//        ],
+        [
+            'class' => 'kartik\grid\DataColumn',
+            'attribute' => 'account'
+        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'original'
+//        ],
+        [
+            'class' => 'kartik\grid\DataColumn',
+            'attribute' => 'type',
+            'format' => 'html',
+            'filter' => Wechat::$types,
+            'value' => function($model) {
+                return Html::tag('span', Wechat::$types[$model->type], [
+                    'class' => 'label label-info'
+                ]);
+            }
+        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'key'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'secret'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'encoding_aes_key'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'type'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'avatar'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'qrcode'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'address'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'description'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'username'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'status'
+//        ],
+//        [
+//            'class' => 'kartik\grid\DataColumn',
+//            'attribute' => 'password'
+//        ],
+        [
+            'class' => 'kartik\grid\DataColumn',
+            'attribute' => 'status',
+            'format' => 'html',
+            'filter' => Wechat::$statuses,
+            'value' => function($model) {
+                return Html::tag('span', Wechat::$statuses[$model->status], [
+                    'class' => 'label label-' . ($model->status == Wechat::STATUS_ACTIVE ? 'success' : 'danger')
+                ]);
+            },
+        ],
+        [
+            'class' => 'kartik\grid\DataColumn',
+            'attribute' => 'created_at',
+            'format' => 'datetime'
+        ],
+        [
+            'class' => 'kartik\grid\DataColumn',
+            'attribute' => 'updated_at',
+            'format' => 'datetime'
+        ],
+        [
+            'class' => 'kartik\grid\ActionColumn',
+            'template' => '{manage} {update} {delete}',
+            'width' => '150px',
+            'buttons' => [
+                'manage' => function ($url, $model) {
+                    return Html::a('管理此公众号', $url, [
+                        'class' => 'text-danger',
+                        'data-pjax' => 0
+                    ]);
+                }
+            ],
+        ]
+    ]
+]);
